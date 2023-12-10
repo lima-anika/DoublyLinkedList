5c12f394e0cfabe44c03da975151884bbd39b955
Anika <48611156+lima-anika@users.noreply.github.com>
2023-12-10 22:37:20 +0600

    Add files via upload

---
+++ linked_doubly_list_dynamic.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int num;
+    Node* prev;
+    Node* next;
+
+    Node(int num)
+    {
+        this->num=num;
+        this->prev=NULL;
+        this->next=NULL;
+    }
+};
+
+void insert_at_tail(Node *&head,Node *&tail,int num)
+{
+    Node *newNode=new Node(num);
+
+    if(head==NULL)
+    {
+        head=newNode;
+        tail=newNode;
+        return;
+    }
+
+    else
+    {
+        tail->next=newNode;
+        tail->next->prev=tail;
+        tail=tail->next;
+    }
+}
+
+void print_linked_list(Node *head)
+{
+    Node *t=head;
+
+    while(t!=NULL)
+    {
+        cout<<t->num<<"  ";
+        t=t->next;
+    }
+}
+
+int main()
+{
+    Node *head=NULL;
+
+    Node *tail=NULL;
+
+    while(1)
+    {
+        int n1;
+
+        cin>>n1;
+
+        if(n1==-1)
+        {
+            break;
+        }
+        insert_at_tail(head,tail,n1);
+    }
+
+    print_linked_list(head);
+
+}
